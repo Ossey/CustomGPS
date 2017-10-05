f83aa0ed3b4b3846dc64272aecfe72f1aac6b7e1
Ossey <xiaoyuan1314@me.com>
2017-10-06 00:39:43 +0800

    Add LocationConverter Class

---
+++ CustomGPS/LocationConverter.h
@@ -0,0 +1,13 @@
+//
+//  LocationConverter.h
+//  CustomGPS
+//
+//  Created by Swae on 2017/10/6.
+//  Copyright © 2017年 Ossey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationConverter : NSObject
+
+@end
