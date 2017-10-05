ef2b3289e3ff2c424d401418386050e94e0a6b89
Ossey <xiaoyuan1314@me.com>
2017-10-06 01:12:55 +0800

    Custom location

--- CustomGPS/ViewController.m
+++ CustomGPS/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LocationConverter.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // 首先需要在配置gpx文件: 进入Product-Edit Scheme-Edit Scheme菜单, 选择Run-Options,在 Core Location项目中选中Allow Location Simulation复选框,然后在 Default Location选择gpx文件
+    
+    // 将需要定位坐标转换为wps坐标
+    CLLocation *location = [[CLLocation alloc] initWithLatitude:30.266732 longitude:119.956855];
+    CLLocationCoordinate2D coord2d = [LocationConverter gcj02ToWgs84:location.coordinate];
+    NSLog(@"纬度: %f, 经度:%f", coord2d.latitude, coord2d.longitude);
+    
+    // 再将转换后的纬度和经度添加到项目中自定义的Location.gpx，放到文件对应的经度和纬度中即可
+    // 将此项目运行到iOS设备上即可
+    
 }
 
 
