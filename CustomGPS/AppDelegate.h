1b7ee0151cad8a2156358b94cf896ee855a999f1
Ossey <xiaoyuan1314@me.com>
2017-10-06 00:53:44 +0800

    Initial project

---
+++ CustomGPS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomGPS
+//
+//  Created by Swae on 2017/10/6.
+//  Copyright © 2017年 Ossey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
