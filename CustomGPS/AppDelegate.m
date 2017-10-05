fce4754f9cc9e8140c4c4de4db31be015674a27f
Ossey <xiaoyuan1314@me.com>
2017-10-06 00:28:47 +0800

    Initial commit

---
+++ CustomGPS/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  CustomGPS
+//
+//  Created by Swae on 2017/10/6.
+//  Copyright © 2017年 Ossey. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ CustomGPS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomGPS
+//
+//  Created by Swae on 2017/10/6.
+//  Copyright © 2017年 Ossey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
